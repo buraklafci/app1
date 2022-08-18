65a55a3c14ef7abcb0b544c48126b4fc388b5c54
BURAK LAFCI <karanfilkaranfil822@gmail.com>
2022-08-18 16:11:33 +0300

    b

---
+++ C01.java
@@ -0,0 +1,13 @@
+package day2_GitGitHub.src.day3_GitGitHub;
+
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("ogrendim galiba bu isi");
+        System.out.println("ogrendim galiba bu isi ...");
+        System.out.println("ogrendim galiba bu isi ......");
+
+        System.out.println("ogrendim galiba bu isi ......resolve");
+
+
+    }
+}
---
+++ Gitignore.java
@@ -0,0 +1,14 @@
+package day2_GitGitHub.src.day3_GitGitHub;
+
+public class Gitignore {
+    public static void main(String[] args) {
+        System.out.println("Gitignore Deneme 1");
+        System.out.println("Gitignore Deneme Branch");
+
+        System.out.println("Gitignore Deneme Branch...");
+        System.out.println("Gitignore Deneme Branch burak...");
+
+        System.out.println("Gitignore Deneme master");
+
+    }
+}
